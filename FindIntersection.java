07f19be1e524b9dc5ef07f189f1f40cc1b0dc5ee
JPujitha <88589553+JPujitha@users.noreply.github.com>
2021-08-25 19:26:58 +0530

    Add files via upload

--- FindIntersection.java
+++ FindIntersection.java
@@ -6,21 +6,42 @@ import java.util.List;
 
 public class FindIntersection {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] s1={3,2,11,4,6,7};
-		int[] s2={1,2,8,4,9,7};
-		Arrays.sort(s1);
-		Arrays.sort(s2);
-		List<Integer> A1= new ArrayList<Integer>();
+	// TODO Auto-generated method stub
+	int[] s1 = { 3, 2, 11, 4, 6, 7 };
+	int[] s2 = { 1, 2, 8, 4, 9, 7 };
+
+	public void m1() {
+		List<Integer> A1 = new ArrayList<Integer>();
 		for (int i = 0; i < s1.length; i++) {
 			for (int j = 0; j < s2.length; j++) {
-				if (s1[i]==s2[j]) {
+				if (s1[i] == s2[j]) {
 					A1.add(s1[i]);
+
 				}
 			}
 		}
-		System.out.println("Intersection elements are : "+A1);
+		System.out.println("Intersection elements from m1 : " + A1);
 	}
 
+	public void m2() {
+		List<Integer> A = new ArrayList<Integer>();
+		List<Integer> B = new ArrayList<Integer>();
+
+		for (int i = 0; i < s1.length; i++) {
+			A.add(s1[i]);
+		}
+		for (int i = 0; i < s2.length; i++) {
+			B.add(s2[i]);
+		}
+		A.retainAll(B);
+		System.out.println("Intersection element from m2 : "+A);
+
+	}
+
+public static void main(String[] args) {
+	FindIntersection ob= new FindIntersection();
+	ob.m1();
+	ob.m2();
+
+}
 }
