eaa1bc2c46c5ccb27cb8bbf89417bd2267b16e89
JPujitha <88589553+JPujitha@users.noreply.github.com>
2021-08-25 22:54:36 +0530

    Add files via upload

--- DuplicateString.java
+++ DuplicateString.java
@@ -1,16 +1,18 @@
 package week3.Day2.Assignmnets;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class DuplicateString {
 
-	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String text = "We learn java basics as part of java sessions in java week1 sessions";
 		String[] str = text.split(" ");
 		List<String> A = new ArrayList<String>();
 		List<String> D = new ArrayList<String>();
+		public void list() {
 		for (int i = 0; i < str.length; i++) {
 			if (A.contains(str[i])) {
 				D.add(str[i]);
@@ -19,6 +21,25 @@ public class DuplicateString {
 				A.add(str[i]);
 			}
 		}
-		System.out.println(A);
-	}
+	
+		System.out.println("String using list : "+A+"\n");
+		
+		}
+		
+		public void set() {
+			Set<String> A= new LinkedHashSet<String>();
+for (int i = 0; i < str.length; i++) {
+	A.add(str[i]);
+	A.remove(false);
+}
+System.out.println("String using set : "+A);
+
+				
+		}
+		public static void main(String[] args) {
+			DuplicateString ob=new DuplicateString();
+			ob.list();
+			ob.set();
+}
+
 }
